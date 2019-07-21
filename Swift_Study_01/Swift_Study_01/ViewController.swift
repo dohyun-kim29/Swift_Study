da2b72f40bfb1ad8f7d512733b2ca1c1f6caa64b
Dohyun-Kim <rlaehgus1229@gmail.com>
2019-07-21 23:45:35 +0900

    Study01

--- Swift_Study_01/Swift_Study_01/ViewController.swift
+++ Swift_Study_01/Swift_Study_01/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sayHello(_ sender: Any) {
+        self.uiTitle.text = "Hello, World!"
+    }
+    
 }
 
