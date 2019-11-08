04ee760844e3787a93941e5ed13c09c2d191aeb3
DohyunKIm <rlaehgus1229@gmail.com>
2019-11-08 16:48:40 +0900

    Study08

---
+++ Swift_Study_08/Swift_Study_08/Page2ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Page2ViewController.swift
+//  Swift_Study_08
+//
+//  Created by DohyunKim on 08/11/2019.
+//  Copyright © 2019 DohyunKim. All rights reserved.
+//
+
+import UIKit
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_segue: UIStoryboardSegue) {
+        
+    }
+}
