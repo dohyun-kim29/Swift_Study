e22a18c96d20ee599061e52b1f11b340530e9677
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-09 18:08:14 +0900

    Study12

--- Swift_Study_12/Swift_Study_12/ViewController.swift
+++ Swift_Study_12/Swift_Study_12/ViewController.swift
@@ -21,11 +21,21 @@ class ViewController: UIViewController {
     
     @IBAction func alert(_ sender: Any) {
         
-        let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .alert)
+        let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .actionSheet)
         
         let cancel = UIAlertAction(title: "취소", style: .cancel)
         
+        let ok = UIAlertAction(title: "확인", style: .default)
+        
+        let exec = UIAlertAction(title: "실행", style: .destructive)
+        
+        let stop = UIAlertAction(title: "중지", style: .default)
+        
+        
         alert.addAction(cancel)
+        alert.addAction(ok)
+        alert.addAction(exec)
+        alert.addAction(stop)
         
         self.present(alert, animated: false)
     }
