7e3d5fd717ed048950f3a7bd9591e02e72d1a198
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-12 17:08:51 +0900

    Study16

---
+++ Swift_Study_16/Swift_Study_16/ListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ListViewController.swift
+//  Swift_Study_16
+//
+//  Created by DohyunKim on 2020/06/12.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+class ListViewController: UITableViewController {
+    
+
+    var dataset = [
+               ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
+               ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
+               ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+           ]
+           
+    lazy var list: [MovieVO] = {
+               var datalist = [MovieVO]()
+            for(title, desc, opendate, rating) in self.dataset {
+                   let mvo = MovieVO()
+                   mvo.title = title
+                   mvo.description = desc
+                   mvo.opendate = opendate
+                   mvo.rating = rating
+                   
+                   datalist.append(mvo)
+               }
+               return datalist
+           }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+       
+    }
+    
+}
---
+++ Swift_Study_16/Swift_Study_16/MovieVO.swift
@@ -0,0 +1,18 @@
+//
+//  MovieVO.swift
+//  Swift_Study_16
+//
+//  Created by DohyunKim on 2020/06/12.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail: String?
+    var title: String?
+    var description: String?
+    var detail: String?
+    var opendate: String?
+    var rating: Double?
+}
