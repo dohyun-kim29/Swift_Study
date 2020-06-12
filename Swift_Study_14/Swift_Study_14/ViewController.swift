1e5398c2c8a1b2de478750438f273df7eb13b51e
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-12 09:59:57 +0900

    Study14

--- Swift_Study_14/Swift_Study_14/ViewController.swift
+++ Swift_Study_14/Swift_Study_14/ViewController.swift
@@ -25,6 +25,19 @@ class ViewController: UIViewController {
         
         self.tf.enablesReturnKeyAutomatically = true
         
+        
+        self.tf.borderStyle = UITextField.BorderStyle.line
+        
+        self.tf.backgroundColor = UIColor(white: 0.87, alpha: 1.0)
+        
+        self.tf.contentVerticalAlignment = .center
+        
+        self.tf.contentHorizontalAlignment = .center
+        
+        self.tf.layer.borderColor = UIColor.darkGray.cgColor
+        
+        self.tf.layer.borderWidth = 2.0
+        
         // Do any additional setup after loading the view.
     }
 
