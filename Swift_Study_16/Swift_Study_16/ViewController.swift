00c10fa1a2006dadc3e85681aad30b34d48739b8
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-12 16:05:17 +0900

    Study16

---
+++ Swift_Study_16/Swift_Study_16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_Study_16
+//
+//  Created by DohyunKim on 2020/06/12.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
