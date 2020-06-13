71ecec03670480541bb8da602d537dfa7612b5ef
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-13 16:59:57 +0900

    Study16

--- Swift_Study_16/Swift_Study_16/ListViewController.swift
+++ Swift_Study_16/Swift_Study_16/ListViewController.swift
@@ -30,6 +30,18 @@ class ListViewController: UITableViewController {
                return datalist
            }()
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = self.list[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
