73b45c2a9663c2548fd2fa3d235a31832ad7f65a
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-15 18:22:33 +0900

    File Replace

--- Swift_Study_16/MyMovieChart/ListViewController.swift
+++ Swift_Study_16/MyMovieChart/ListViewController.swift
@@ -10,20 +10,21 @@ import UIKit
 class ListViewController: UITableViewController {
   // 튜플 아이템으로 구성된 데이터 세트
   var dataset = [
-    ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
-    ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-    ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+    ("다크나이트", "영웅물에 철학에..예술이 되다", "2008-09-04", 8.95, "darknight.jpg"),
+    ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+    ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
   ]
   
   // 테이블 뷰를 구성할 리스트 데이터
   lazy var list : [MovieVO] = {
     var datalist = [MovieVO]()
-    for (title, desc, opendate, rating) in self.dataset {
+    for (title, desc, opendate, rating, thumbnail) in self.dataset {
       let mvo = MovieVO()
       mvo.title = title
       mvo.description = desc
       mvo.opendate = opendate
       mvo.rating = rating
+      mvo.thumbnail = thumbnail
       
       datalist.append(mvo)
     }
@@ -37,29 +38,24 @@ class ListViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     // 주어진 행에 맞는 데이터 소스를 읽어온다.
     let row = self.list[indexPath.row]
-    // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
-   
-    let title = cell.viewWithTag(101) as? UILabel
     
-    let desc = cell.viewWithTag(102) as? UILabel
+    // ========= 여기부터 내용 변경됨 =========
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
     
-    let opendate = cell.viewWithTag(103) as? UILabel
+    // 데이터 소스에 저장된 값을 각 아울렛 변수에 할당
+    cell.title?.text = row.title
+    cell.desc?.text = row.description
+    cell.opendate?.text = row.opendate
+    cell.rating?.text = "\(row.rating!)"
     
-    let rating = cell.viewWithTag(104) as? UILabel
-    
-    title?.text = row.title
-    desc?.text = row.description
-    opendate?.text = row.opendate
-    rating?.text = "\(row.rating!)"
+    // 추가된 부분: 이미지 뷰 처리
+    cell.thumbnail.image = UIImage(named: row.thumbnail!)
+    // ========= 여기까지 내용 변경됨 =========
     
     return cell
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
-    
   }
-    
-
 }
