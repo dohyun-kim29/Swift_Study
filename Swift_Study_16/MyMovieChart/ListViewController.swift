ca3c9ca2b6e39ea7891cf6a9fe0ff2d3c2d43caf
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-16 13:10:50 +0900

    Study16
    error

--- Swift_Study_16/MyMovieChart/ListViewController.swift
+++ Swift_Study_16/MyMovieChart/ListViewController.swift
@@ -14,37 +14,8 @@ class ListViewController: UITableViewController {
   // 테이블 뷰를 구성할 리스트 데이터
   lazy var list : [MovieVO] = {
     var datalist = [MovieVO]()
-    }
     return datalist
   }()
-
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.list.count
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // 주어진 행에 맞는 데이터 소스를 읽어온다.
-    let row = self.list[indexPath.row]
-    
-    // ========= 여기부터 내용 변경됨 =========
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
-    
-    // 데이터 소스에 저장된 값을 각 아울렛 변수에 할당
-    cell.title?.text = row.title
-    cell.desc?.text = row.description
-    cell.opendate?.text = row.opendate
-    cell.rating?.text = "\(row.rating!)"
-    
-    // 추가된 부분: 이미지 뷰 처리
-    cell.thumbnail.image = UIImage(named: row.thumbnail!)
-    // ========= 여기까지 내용 변경됨 =========
-    
-    return cell
-  }
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
-  }
     
     override func viewDidLoad() {
         let url = "https://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=1&count=10&genreId=&order=releasedateasc"
@@ -53,7 +24,7 @@ class ListViewController: UITableViewController {
         
         let apidata = try! Data(contentsOf: apiURI)
         
-        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? ""
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "데이터가 없습니다"
             NSLog("API Result=\(log)")
         
         do {
@@ -78,5 +49,33 @@ class ListViewController: UITableViewController {
             }
             
         } catch { }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      // 주어진 행에 맞는 데이터 소스를 읽어온다.
+      let row = self.list[indexPath.row]
+      
+      // ========= 여기부터 내용 변경됨 =========
+      let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
+      
+      // 데이터 소스에 저장된 값을 각 아울렛 변수에 할당
+      cell.title?.text = row.title
+      cell.desc?.text = row.description
+      cell.opendate?.text = row.opendate
+      cell.rating?.text = "\(row.rating!)"
+      
+      // 추가된 부분: 이미지 뷰 처리
+      cell.thumbnail.image = UIImage(named: row.thumbnail!)
+      // ========= 여기까지 내용 변경됨 =========
+      
+      return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
     }
 }
