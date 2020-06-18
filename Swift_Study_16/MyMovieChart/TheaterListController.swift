a889ddb3e65a8ff93fffdb7549873503fa2cd71d
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-18 20:19:40 +0900

    Study16

--- Swift_Study_16/MyMovieChart/TheaterListController.swift
+++ Swift_Study_16/MyMovieChart/TheaterListController.swift
@@ -15,6 +15,8 @@ class TheaterListController: UITableViewController {
     
     override func viewDidLoad() {
         self.callTheatorAPI()
+        
+        
     }
     
     func callTheatorAPI() {
@@ -23,6 +25,52 @@ class TheaterListController: UITableViewController {
         let sList = 100
         let type = "json"
         
-        let urlObj = URL(string: "\(requestURI)?s_page=\(self.startPoint)")
+        let urlObj = URL(string: "\(requestURI)?s_page=\(self.startPoint)&s_list=\(sList)&type=\(type)")
+        
+        do {
+            
+            let stringdata = try NSString(contentsOf: urlObj!, encoding: 0x80_000_422)
+            
+            let encdata = stringdata.data(using: String.Encoding.utf8.rawValue)
+            
+            do {
+                
+                let apiArray = try JSONSerialization.jsonObject(with: encdata!, options: []) as? NSArray
+                
+                for obj in apiArray! {
+                    self.list.append(obj as! NSDictionary)
+                }
+            } catch {
+                let alert = UIAlertController(title: "실패", message: "데이터 분석이 실패하였습니다", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+                    self.present(alert, animated: false)
+            }
+            self.startPoint += sList
+        } catch {
+            let alert = UIAlertController(title: "실패", message: "데이터를 불러오는데 실패하였습니다", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "확인" , style: .cancel))
+            self.present(alert, animated: false)
+        }
+        
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let obj = self.list[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tCell") as! ThearterCell
+        cell.name?.text = obj["상영관명"] as? String
+        cell.tel?.text = obj["연락처"] as? String
+        cell.addr?.text = obj["소재지도로명주소"] as? String
+        
+        return cell
+        
     }
 }
