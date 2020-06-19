ec095090130863dd539c3722ef0035bea7cc82a3
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-19 09:00:57 +0900

    Study16

---
+++ Swift_Study_16/MyMovieChart/TheaterViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TheaterViewController.swift
+//  MyMovieChart
+//
+//  Created by DohyunKim on 2020/06/19.
+//  Copyright © 2020 SQLPRO. All rights reserved.
+//
+
+import UIKit
+class TheaterViewController: UIViewController {
+    var param: NSDictionary!
+}
