c3827285de633be7c106d6f54e2af7e221d533fe
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-19 09:24:45 +0900

    Study16

--- Swift_Study_16/MyMovieChart/TheaterViewController.swift
+++ Swift_Study_16/MyMovieChart/TheaterViewController.swift
@@ -7,6 +7,27 @@
 //
 
 import UIKit
+import MapKit
 class TheaterViewController: UIViewController {
+    
+    @IBOutlet weak var map: MKMapView!
+    
     var param: NSDictionary!
+    
+    override func viewDidLoad() {
+        self.navigationItem.title = self.param["상영관명"] as? String
+        
+        let lat = (param?["위도"] as! NSString).doubleValue
+        
+        let lng = (param?["경도"] as! NSString).doubleValue
+        
+        
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+        
+        let regionRadius: CLLocationDistance = 100
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location, regionRadius, regionRadius)
+        
+        self.map.setRegion(coordinateRegion, animated: true)
+    }
 }
